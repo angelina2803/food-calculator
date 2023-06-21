f8cb7081cf1bc3ce680ca4ff05c5219a2cf11003
Angelina Yumatova <>
2023-06-21 19:07:43 +0200

    .

--- src/pages/CalculateForm.js
+++ src/pages/CalculateForm.js
@@ -6,6 +6,8 @@ import Card from '@mui/material/Card';
 
 const CalculateForm = () => {
     return (
+      <div className="conteinerCard">
+      <h1>Caterogie</h1>
         <div className="cardGrid">
       <Card sx={{ maxWidth: 345 }}>
       <img src="./img/ovoce/photo1.jpg" alt="Cherry" />
@@ -43,6 +45,7 @@ const CalculateForm = () => {
       <img src="./img/ovoce/photo3.jpg" alt="Banan" />
       <p>Banan</p>
       </Card>
+    </div>
     </div>
     );
 };
